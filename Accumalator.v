fd38c503e5e2cd9b3bba3e99a7ad72519bd60f83
Hung Ta <khanghung711@gmail.com>
2025-04-08 16:19:22 +0700

    Adding testbench file
    Testbench for Accumulator and Instruction memory

--- Accumalator.v
+++ Accumalator.v
@@ -1,17 +1,15 @@
-module Accumalator(
+module Accumulator(
     input clk,
     input reset,
     input en_acc_in,
     input [7:0] alu_result,
-    output [7:0] acc_out
-    );
-    reg [7:0] Accumulator;
-
-    always @(posedge clk, posedge reset) begin
-        if(reset) Accumulator <= 8'd0;
-        else if(en_acc) Accumulator <= result;
-   end
-
-    assign alu_result = Accumulator;
+    output reg [7:0] acc_out
+);
     
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            acc_out <= 8'd0;
+        else if (en_acc_in)
+            acc_out <= alu_result;
+    end
 endmodule
