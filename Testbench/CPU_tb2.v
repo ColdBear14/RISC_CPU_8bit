8ac592f97948c93e2b84eae9e9145a801ac03886
LAPTOP-L4QLQTHA\Hy <ngxuanhy1405@gmail.com>
2025-04-13 22:44:11 +0700

    update testbench prog2,3

---
+++ Testbench/CPU_tb2.v
@@ -0,0 +1,74 @@
+module CPU_tb_2(
+
+    );
+    //Input
+    reg clock;
+    reg Load;
+    reg reset;
+    reg [7:0] data_in;
+    
+    //Output
+    wire [7:0] Instruction;
+    wire [7:0] Acc;
+    wire [7:0] Mem;
+    wire [4:0] Program_counter;
+    
+    CPU uut (
+        .clock(clock),
+        .Load(Load),
+        .reset(reset),
+        .data_in(data_in),
+        .Instruction(Instruction),
+        .Acc(Acc),
+        .Mem(Mem),
+        .Program_counter(Program_counter)
+    );
+    
+
+    
+    initial begin
+        clock = 0;
+        reset = 1;
+        Load = 0;
+        data_in = 8'b0;
+        
+        // Reset the CPU
+        #10 reset = 0;
+        
+        Load = 1;
+        
+        // Load instructions into instruction memory
+        #10 data_in = 8'b10111011; // @00: LDA DATA_2
+        #10 data_in = 8'b01111100; // @01: AND DATA_3
+        #10 data_in = 8'b10011011; // @02: XOR DATA_2
+        #10 data_in = 8'b00100000; // @03: SKZ
+        #10 data_in = 8'b00000000; // @04: HLT
+        #10 data_in = 8'b01011010; // @05: ADD DATA_1
+        #10 data_in = 8'b00100000; // @06: SKZ
+        #10 data_in = 8'b11101001; // @07: JMP ADD_OK
+        #10 data_in = 8'b00000000; // @08: HLT
+        #10 data_in = 8'b10011100; // @09: XOR DATA_3
+        #10 data_in = 8'b01011010; // @0A: ADD DATA_1
+        #10 data_in = 8'b11011101; // @0B: STO TEMP
+        #10 data_in = 8'b10111010; // @0C: LDA DATA_1
+        #10 data_in = 8'b01011101; // @0D: ADD TEMP
+        #10 data_in = 8'b00100000; // @0E: SKZ
+        #10 data_in = 8'b00000000; // @0F: HLT
+        #10 data_in = 8'b00000000; // @10: HLT
+        #10 data_in = 8'b11100000; // @11: JMP BEGIN
+        #10 data_in = 8'b00000001; // @1A: DATA_1
+        #10 data_in = 8'b10101010; // @1B: DATA_2
+        #10 data_in = 8'b11111111; // @1C: DATA_3
+        #10 data_in = 8'b00000000; // @1D: TEMP
+
+        Load = 0;
+
+        // Run the simulation
+        #200
+        
+        $finish;
+    end
+    
+    always #5 clock = ~clock;
+
+endmodule
---
+++ Testbench/CPU_tb3.v
@@ -0,0 +1,78 @@
+module CPU_tb_3(
+
+    );
+    //Input
+    reg clock;
+    reg Load;
+    reg reset;
+    reg [7:0] data_in;
+    
+    //Output
+    wire [7:0] Instruction;
+    wire [7:0] Acc;
+    wire [7:0] Mem;
+    wire [4:0] Program_counter;
+    
+    CPU uut (
+        .clock(clock),
+        .Load(Load),
+        .reset(reset),
+        .data_in(data_in),
+        .Instruction(Instruction),
+        .Acc(Acc),
+        .Mem(Mem),
+        .Program_counter(Program_counter)
+    );
+    
+
+    
+    initial begin
+        clock = 0;
+        reset = 1;
+        Load = 0;
+        data_in = 8'b0;
+        
+        // Reset the CPU
+        #10 reset = 0;
+        
+        Load = 1;
+        
+        // Load instructions into instruction memory
+        #10 data_in = 8'b11100011; // @00: JMP LOOP
+        #10 data_in = 8'b00000000; // @01: Unused
+        #10 data_in = 8'b00000000; // @02: Unused
+        #10 data_in = 8'b10111011; // @03: LDA FN2
+        #10 data_in = 8'b11011100; // @04: STO TEMP
+        #10 data_in = 8'b01011010; // @05: ADD FN1
+        #10 data_in = 8'b11011011; // @06: STO FN2
+        #10 data_in = 8'b10111100; // @07: LDA TEMP
+        #10 data_in = 8'b11011010; // @08: STO FN1
+        #10 data_in = 8'b10011101; // @09: XOR LIMIT
+        #10 data_in = 8'b00100000; // @0A: SKZ
+        #10 data_in = 8'b11100011; // @0B: JMP LOOP
+        #10 data_in = 8'b00000000; // @0C: HLT
+        #10 data_in = 8'b10111111; // @0D: LDA ONE
+        #10 data_in = 8'b11011010; // @0E: STO FN1
+        #10 data_in = 8'b10111110; // @0F: LDA ZERO
+        #10 data_in = 8'b11011011; // @10: STO FN2
+        #10 data_in = 8'b11100011; // @11: JMP LOOP
+
+        // Load data into memory
+        #10 data_in = 8'b00000001; // @1A: FN1 = 1
+        #10 data_in = 8'b00000000; // @1B: FN2 = 0
+        #10 data_in = 8'b00000000; // @1C: TEMP = 0
+        #10 data_in = 8'b10010000; // @1D: LIMIT = 144 (decimal)
+        #10 data_in = 8'b00000000; // @1E: ZERO = 0
+        #10 data_in = 8'b00000001; // @1F: ONE = 1
+
+        Load = 0;
+
+        // Run the simulation
+        #200
+        
+        $finish;
+    end
+    
+    always #5 clock = ~clock;
+
+endmodule
